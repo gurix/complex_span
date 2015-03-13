fdecbf9bb9f014fee001fb9c5c7df573c840f906
Markus Graf <info@markusgraf.ch>
2015-03-13 20:50:19 +0100

    fixed nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
-
   def exact_time(date_time)
     # Formating according ISO 8601
-    date_time.strftime('%Y-%m-%dT%H:%M:%S.%L%Z')
+    date_time.strftime('%Y-%m-%dT%H:%M:%S.%L%Z') if date_time
   end
-
 end
