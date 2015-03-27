445af9c52bf22e9fcbf44b52ccd2a62bbe8f409d
Markus Graf <info@markusgraf.ch>
2015-03-27 18:19:13 +0100

    create first session then update data after experiment is done sucessful

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,19 +2,18 @@ class Session
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :age, type: Integer
-  field :sincerity, type: String
-  field :gender
-  field :education
+  field :age,                type: Integer
+  field :sincerity,          type: String
+  field :gender,             type: String
+  field :education,          type: String
   field :system_information, type: Hash
-  field :trials, type: Array
-  field :ip_address, type: String
-  field :language, type: String
+  field :ip_address,         type: String
+  field :language,           type: String
 
   embeds_many :logs
   embeds_many :trials
 
   accepts_nested_attributes_for :logs, :trials
 
-  validates :age, :gender, :education, :system_information, :sincerity, :logs, :trials, :ip_address, :language, presence: true
+  validates :system_information, :logs, :ip_address, :language,  presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'home#index'
 
-  resources :sessions, only: [:create, :index]
+  resources :sessions, only: [:create, :update, :index]
   resources :presentations, only: [:index]
   resources :retrieval_clicks, only: [:index]
   resources :retrievals, only: [:index]
