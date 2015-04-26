a5269aac4fbac1b3c55763ba8c3d157b94aa4b2a
Markus Graf <info@markusgraf.ch>
2015-04-26 18:06:24 +0200

    added the rest of the controllers

--- spec/controllers/retrieval_clicks_controller_spec.rb
+++ spec/controllers/retrieval_clicks_controller_spec.rb
@@ -12,13 +12,13 @@ describe RetrievalClicksController do
       csv = CSV.parse response.body,  converters: :numeric
 
       expect(csv.first).to eq %w(session_id trial word_position retrieval_position click_order clicked_at color delay text word_id size_difference
-                                 retrieval_matrix_shown_at)
+                                 retrieval_matrix_shown_at trial_repeated)
 
       click = session.trials.last.retrieval_clicks.last
 
       expect(csv.last).to eq [click.trials.sessions.id.to_s, click.trial, click.word_position, click.retrieval_position, click.click_order,
                               exact_time(click.clicked_at), click.color, click.delay, click.text, click.word_id, click.size_difference,
-                              exact_time(click.trials.retrieval_matrix_shown_at)]
+                              exact_time(click.trials.retrieval_matrix_shown_at), click.trials.repeated.to_s]
     end
   end
 end
